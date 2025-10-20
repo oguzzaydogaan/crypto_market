4bdd747847abbc8703dede6edb40d8ee1fdaf1bd
Oğuzhan Aydoğan <negatif25@gmail.com>
2025-10-20 19:28:50 +0300

    no message

--- lib/views/pages/favorites_page.dart
+++ lib/views/pages/favorites_page.dart
@@ -11,46 +11,48 @@ class FavoritesPage extends StatefulWidget {
 class _FavoritesPageState extends State<FavoritesPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(onPressed: () {}, icon: Icon(Icons.star)),
-                    Icon(Icons.currency_bitcoin),
-                    Text('Bitcoin (BTC)'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('\$97,256'),
-                    SizedBox(width: 8),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return CoinPage();
-                            },
-                          ),
-                        );
-                      },
-                      icon: Icon(Icons.arrow_forward_ios),
-                    ),
-                  ],
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(onPressed: () {}, icon: Icon(Icons.star)),
+                      Icon(Icons.currency_bitcoin),
+                      Text('Bitcoin (BTC)'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text('\$97,256'),
+                      SizedBox(width: 8),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return CoinPage();
+                              },
+                            ),
+                          );
+                        },
+                        icon: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -11,90 +11,92 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.star_border_outlined),
-                    ),
-                    Icon(Icons.currency_bitcoin),
-                    Text('Bitcoin (BTC)'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('\$100,000'),
-                    SizedBox(width: 8),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return CoinPage();
-                            },
-                          ),
-                        );
-                      },
-                      icon: Icon(Icons.arrow_forward_ios),
-                    ),
-                  ],
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.star_border_outlined),
+                      ),
+                      Icon(Icons.currency_bitcoin),
+                      Text('Bitcoin (BTC)'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text('\$100,000'),
+                      SizedBox(width: 8),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return CoinPage();
+                              },
+                            ),
+                          );
+                        },
+                        icon: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(onPressed: () {}, icon: Icon(Icons.star)),
-                    Icon(Icons.currency_pound),
-                    Text('Ethereum (ETH)'),
-                  ],
-                ),
-                Text('\$30,000'),
-              ],
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(onPressed: () {}, icon: Icon(Icons.star)),
+                      Icon(Icons.currency_pound),
+                      Text('Ethereum (ETH)'),
+                    ],
+                  ),
+                  Text('\$30,000'),
+                ],
+              ),
             ),
           ),
-        ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.star_border_outlined),
-                    ),
-                    Icon(Icons.currency_franc),
-                    Text('Doge (DOGE)'),
-                  ],
-                ),
-                Text('\$0.25'),
-              ],
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.star_border_outlined),
+                      ),
+                      Icon(Icons.currency_franc),
+                      Text('Doge (DOGE)'),
+                    ],
+                  ),
+                  Text('\$0.25'),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
